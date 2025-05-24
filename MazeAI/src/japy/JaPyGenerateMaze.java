d60dab5617838eb5d61b0c9354911f1b7a748d25
chi-dan-min <chiidan2k4@gmail.com>
2025-05-24 12:21:20 +0700

    update japy

--- MazeAI/src/japy/JaPyGenerateMaze.java
+++ MazeAI/src/japy/JaPyGenerateMaze.java
@@ -24,5 +24,16 @@ public class JaPyGenerateMaze {
         } else {
             return null;
         }
-    }    
+    }
+    public Maze regenerateMaze(int posX, int posY, int goalX, int goalY) {
+        jaPy.createPythonProcess();
+        StringBuilder sb = new StringBuilder();
+        sb.append(posX).append(" ").append(posY).append("\n");
+        sb.append(goalX).append(" ").append(goalY).append("\n");
+        Pair<String, Boolean> result = jaPy.runPythonScript(sb.toString());
+
+        // process here
+        
+        return new Maze(null);
+    }
 }
--- MazeAI/src/japy/JaPySolveMaze.java
+++ MazeAI/src/japy/JaPySolveMaze.java
@@ -1,5 +1,10 @@
 package japy;
 
+import java.io.IOException;
+
+import mazenv.Maze;
+import mazenv.Pair;
+
 public class JaPySolveMaze {
     private static JaPy jaPy;
 
@@ -7,8 +12,47 @@ public class JaPySolveMaze {
         jaPy = new JaPy(pythonScriptFilePath, true);
     }
 
-    public void start()
-    {
-
+    public static String generateMazeInput(int[][] discovered, int posX, int posY) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(posX).append(" ").append(posY).append("\n\n");
+        for (int[] row : discovered) {
+            for (int val : row) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+        }
+        sb.append("\n");
+        return sb.toString();
     }
+
+    public static String generateMazeAndSkillsInput(int[][] discovered, int posX, int posY, int[] skillIds) {
+        StringBuilder sb = new StringBuilder(generateMazeInput(discovered, posX, posY));
+        for (int id : skillIds) {
+            sb.append(id).append(" ");
+        }
+        sb.append("\n");
+        return sb.toString();
+    }
+
+
+    public int requestNextStep(int[][] discovered, int posX, int posY) {
+        jaPy.createPythonProcess();
+        String input = generateMazeInput(discovered, posX, posY);
+        Pair<String, Boolean> result = jaPy.runPythonScript(input);
+        
+        // process here
+
+        return 1;
+    }
+
+    public int requestNextSkill(int[][] discovered, int posX, int posY, int[] skillIds) {
+        jaPy.createPythonProcess();
+        String input = generateMazeAndSkillsInput(discovered, posX, posY, skillIds);
+        Pair<String, Boolean> result = jaPy.runPythonScript(input);
+        
+        // process here
+
+        return 1;
+    }
+    
 }
